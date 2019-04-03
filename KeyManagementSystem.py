7a51138b08684fae01be9745af370021bc415028
ShaunJose <shaun.jose12@gmail.com>
2019-04-03 20:46:33 +0100

    Saving key in file and initialise key method added

--- KeyManagementSystem.py
+++ KeyManagementSystem.py
@@ -11,7 +11,24 @@ class KMS:
 
     # Constructor
     def __init__(self):
-        self.__generate_new_key__()
+        self.__init_key__()
+        print(self.key)
+
+
+    # Initialises a new key if it doesn't exist
+    def __init_key__(self):
+        """
+        Initialises a new key if it doesn't exist
+
+        return: None
+        """
+
+        # read key if key exists else generate a new one
+        if os.path.exists(constants.SHARED_KEY_FILE):
+            fileRead = open(constants.SHARED_KEY_FILE, "r")
+            self.key = fileRead.read()
+        else:
+            self.__generate_new_key__()
 
 
     # Generate and save a key
@@ -25,8 +42,11 @@ class KMS:
         # generate a key
         self.key = Fernet.generate_key()
 
-        # save generated key to file in proper directory
+        # create file-keeping directory if it doesn't exist
         if not os.path.isdir(constants.FOLDER_NAME):
             os.mkdir(constants.FOLDER_NAME)
 
-        
+        # save key to appropraite file in directory
+        filepath = open(constants.SHARED_KEY_FILE, "w")
+        filepath.write(self.key)
+        filepath.close()
--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 # Folder name where all files will be saved
 FOLDER_NAME = "Files"
 
-# File name where key is saved
-KEY_FILE = FOLDER_NAME + "/Symmetric Key"
+# File name where the symmetric key is saved
+SHARED_KEY_FILE = FOLDER_NAME + "/Shared Key"
